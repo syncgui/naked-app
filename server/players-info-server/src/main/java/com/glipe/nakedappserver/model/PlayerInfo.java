ada38e57fbc33fe52c15c59dca30c3ded59e7bdf
Guilherme Yuri <guilhermeyla@gmail.com>
2023-12-28 22:38:18 -0300

    Players info server: Personal ID availability check

--- server/players-info-server/src/main/java/com/glipe/nakedappserver/model/PlayerInfo.java
+++ server/players-info-server/src/main/java/com/glipe/nakedappserver/model/PlayerInfo.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class PlayerInfo {
     private String playerID;
     private String name;
-    private String age;
+    private int age;
     private String contactPhone;
     private Double rating = null;
 }
